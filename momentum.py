dbe6fde7e10033745815f581358f38c2d04e6897
JonatanKarni165 <yonatankarni165@gmail.com>
2023-12-20 22:50:05 +0200

    balls dont slide of screen

--- momentum.py
+++ momentum.py
@@ -17,11 +17,11 @@ class Ball(pygame.sprite.Sprite):
         global static_ball_counter
         self.id = static_ball_counter
         static_ball_counter += 1
-
         self.velocity = [0.0, 0.0]
         self.speed = 0
         self.collision_cooldown = 5
         self.debug_mode = debug_mode
+        self.is_colliding = False
 
         if radius:
             self.radius = radius
@@ -56,7 +56,7 @@ class Ball(pygame.sprite.Sprite):
         self.speed = 0
 
     def update(self):
-        # print('i am ball num:', self.id, 'my velocity is:', self.velocity)
+        print('ball num:', self.id, 'position', [self.x, self.y], 'velocity:', self.velocity)
         self.collision_cooldown -= 1
         self.x += self.velocity[0]
         self.y += self.velocity[1]
@@ -67,10 +67,14 @@ class Ball(pygame.sprite.Sprite):
             (self.x + self.radius > WINDOW_SCALE[0] and self.velocity[0] > 0)
         vertical_flip = (self.y - self.radius < 0 and self.velocity[1] < 0) or \
             (self.y + self.radius > WINDOW_SCALE[1] and self.velocity[1] > 0)
-        if horizontal_flip and self.speed > 0.1:
+        if horizontal_flip:
             self.velocity[0] *= -1
-        if vertical_flip and self.speed > 0.1:
+        if vertical_flip:
             self.velocity[1] *= -1
+        if horizontal_flip or vertical_flip:
+            self.is_colliding = True
+        if not horizontal_flip and not vertical_flip:
+            self.is_colliding = False
 
     def draw(self):
         pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius)
@@ -81,12 +85,13 @@ class Ball(pygame.sprite.Sprite):
 
 
 class Ball_To_Ball_Interaction:
-    def __init__(self, first_ball: Ball, second_ball: Ball):
+    def __init__(self, first_ball: Ball, second_ball: Ball, decay_rate):
         self.first_ball = first_ball
         self.second_ball = second_ball
         self.min_distance = first_ball.radius + second_ball.radius
         self.offset = 3
         self.consecutive_call_counter = 0
+        self.decay_rate = decay_rate
 
         self.small_ball, self.big_ball = second_ball, first_ball
         if first_ball.mass < second_ball.mass:
@@ -100,35 +105,45 @@ class Ball_To_Ball_Interaction:
 
     def check_collision(self):
         if self.distance < (self.min_distance + self.offset):
+            self.first_ball.is_colliding = True
+            self.second_ball.is_colliding = True
             self.bounce_opposite_directions()
         else:
+            self.first_ball.is_colliding= True
+            self.second_ball.is_colliding = True
             self.consecutive_call_counter = 0
 
     def bounce_opposite_directions(self):
         if self.consecutive_call_counter > 1:
-            print('block collision')
+            #print('block collision')
             return
 
         if self.consecutive_call_counter == 1:
             self.small_ball.velocity = [
                 -self.small_ball.velocity[0], -self.small_ball.velocity[1]]
             self.consecutive_call_counter += 1
-            print('flip small velocity')
+            #print('flip small velocity')
             return
 
-        print('first collision:\n')
-        print('small ball velocity before:', self.small_ball.velocity)
-        print('big ball velocity before:', self.big_ball.velocity)
+        #print('first collision:\n')
+        #print('small ball velocity before:', self.small_ball.velocity)
+        #print('big ball velocity before:', self.big_ball.velocity)
 
-        temp_momentum = self.first_ball.get_momentum().copy()
+        big_momentum = self.first_ball.get_momentum().copy()
+        small_momentum = self.second_ball.get_momentum().copy()
+
+        # decay energy
+        big_momentum = vector_multiplication(big_momentum, self.decay_rate)
+        small_momentum = vector_multiplication(small_momentum, self.decay_rate)
+
         self.first_ball.set_zero_speed()
-        self.first_ball.add_force(self.second_ball.get_momentum().copy())
+        self.first_ball.add_force(small_momentum)
 
         self.second_ball.set_zero_speed()
-        self.second_ball.add_force(temp_momentum)
+        self.second_ball.add_force(big_momentum)
 
-        print('\nsmall ball velocity after:', self.small_ball.velocity)
-        print('big ball velocity after:', self.big_ball.velocity)
+        #print('\nsmall ball velocity after:', self.small_ball.velocity)
+        #print('big ball velocity after:', self.big_ball.velocity)
 
         self.consecutive_call_counter += 1
 
@@ -146,8 +161,9 @@ def get_distance_between_two_balls(first_ball, second_ball):
 
 
 class Plane:
-    def __init__(self, amount=None, speed=None, radius=None, randomize_radius=False):
-
+    def __init__(self, amount=None, speed=None, radius=None, randomize_radius=False, decay_rate=1, gravity_scale=0):
+        self.decay_rate = decay_rate
+        self.gravity_vector = vector_multiplication([0,1], gravity_scale)
         if amount is None:
             self.balls: list[Ball] = []
             self.ball_interactions: list[Ball_To_Ball_Interaction] = []
@@ -165,7 +181,7 @@ class Plane:
         for i in range(len(self.balls)):
             for j in range(i+1, len(self.balls)):
                 interaction = Ball_To_Ball_Interaction(
-                    self.balls[i], self.balls[j])
+                    self.balls[i], self.balls[j], decay_rate=self.decay_rate)
                 self.ball_interactions.append(interaction)
 
     def add_ball(self, ball):
@@ -175,18 +191,20 @@ class Plane:
         for i in range(len(self.balls)):
             for j in range(i+1, len(self.balls)):
                 interaction = Ball_To_Ball_Interaction(
-                    self.balls[i], self.balls[j])
+                    self.balls[i], self.balls[j], decay_rate=self.decay_rate)
                 self.ball_interactions.append(interaction)
 
     def update_screen(self):
         screen.fill((0, 0, 0))
-
+        print()
         for interaction in self.ball_interactions:
             interaction.update_interaction()
             interaction.check_collision()
 
         for ball in self.balls:
             ball.update()
+            if ball.y < WINDOW_SCALE[1]-ball.radius and not ball.is_colliding:
+                ball.add_force(self.gravity_vector)
             ball.draw()
         pygame.display.update()
 
@@ -195,8 +213,8 @@ def three_size_ball_plane():
     plane = Plane()
     small_ball = Ball(10, spawn_point=(10, 10))
     small_ball.add_force((10, -10))
-    big_ball = Ball(30, spawn_point=(100, 100))
-    big_ball.add_force((2, 2))
+    big_ball = Ball(60, spawn_point=(300, 300))
+    big_ball.add_force((10, -10))
     medium_ball = Ball(20, spawn_point=(50, 50))
     medium_ball.add_force((-2, -2))
     plane.add_ball(small_ball)
@@ -222,7 +240,7 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     run = True
-    plane = small_particles_plane()
+    plane = Plane(7, 20, 20, True, 0.8, 3)
     while (run):
         global FPS
         clock.tick(FPS)
